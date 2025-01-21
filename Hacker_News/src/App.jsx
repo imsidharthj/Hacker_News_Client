27f33aeb3004cb414e597133ba2040a3eae143d4
Sidharth Jain <imsidharthj@gmail.com>
2025-01-21 22:28:36 +0530

    Hacker Rank Client made different for function for rendering one api data at one time

--- Hacker_News/src/App.jsx
+++ Hacker_News/src/App.jsx
@@ -6,42 +6,55 @@ import Footer from './Footer';
 function App() {
   const [topStories, setTopStories] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const [OldApitopStories, setOldTopStories] = useState([]);
   useEffect(() => {
-    fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        const top5StoryIds = data.slice(0, 5);
-        return Promise.all(
-          top5StoryIds.map(id =>
-            fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(res => res.json())
-          )
-        );
-      })
-      .then(stories => {
-        setTopStories(stories);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      });
+    fetchApi()
   }, []);
+
+  const fetchApi = () => {
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const top5StoryIds = data.slice(0, 5);
+      return Promise.all(
+        top5StoryIds.map(id =>
+          fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(res => res.json())
+        )
+      );
+    })
+    .then(stories => {
+      setTopStories(stories);
+      setLoading(false);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    });
+  }
 
   if (loading) {
     return <div>Loading...</div>;
   }
-
+ 
+  const navClick = (label) => {
+    if(label === null){
+      setOldTopStories(topStories)
+      setTopStories([])
+    }
+    else{
+      setTopStories(OldApitopStories)
+    }
+  }
   return (
     <div className="font-sans min-h-screen flex flex-col items-center bg-gray-100 text-gray-800">
-      <NavBar />
+      <NavBar onNavClick={navClick}/>
       <main className="w-full max-w-4xl mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-6 text-center">Hacker News Top Stories</h1>
+        { topStories.length > 0 && <h1 className="text-3xl font-bold mb-6 text-center">Hacker News Top Stories</h1>}
         <ul className="space-y-6">
           {topStories.map((story) => (
             <li
